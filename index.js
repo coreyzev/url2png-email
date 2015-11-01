91a16bf2ffb34bcb869cb972536909dfb94398df
Corey Zev <czwolf@gmail.com>
2015-10-31 19:58:49 -0700

    adding unique time to image & updating options

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/bin/bash node
 
 var fs = require('fs');
-var exec = require('child_process').exec;
+var exec = require('child_process').execSync;
 
 var API_KEY = 'XXXXXXXXXXXX'; // your url2png APIKEY
 var PRIVATE_KEY = 'XXXXXXXXXXXX'; // your url2png SECRET
@@ -15,16 +15,24 @@ var options = {
     viewport: '940x580',
     thumbnail_max_width : 940,
     protocol: 'https',
-    delay: 10
+    delay: 10,
+    unique: Date.now(),
+    ttl: 60 * 60 * 24 * 2
 };
 
+var time = exec('date +%Y-%m-%d_%H-%M');
+var image_name = 'wag'; // name your file
+var img_file = image_name + '_' + time + '.png';
+img_file = img_file.replace(/[\s\?]/gm,'');
+
 var url = 'google.com'; // the url you wish to have screenshotted and emailed to you
-var filename = 'image.png'; //name your image
 var recipient = 'name@example.org'; // email to recieve the picture
 var subject = "Subject Line"; // Subject line, be careful to escape quotes
 
-var stream = url2png.readURL(url, options).pipe(fs.createWriteStream(filename));
+
+var stream = url2png.readURL(url, options).pipe(fs.createWriteStream(img_file));
 
 stream.on('finish', function() {
-    exec('mutt -a ' + filename + ' -s "' + subject + '" -- ' + recipient + ' < /tmp/message.txt');
+    exec('mutt -a ' + img_file + ' -s "' + subject + '" -- ' + recipient + ' < /tmp/message.txt');
+    exec('rm '+ img_file);
 });
